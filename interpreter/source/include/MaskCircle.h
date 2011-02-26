3b08c0d0ebe8f24b70d03da4a209f2491f86b274
ritrix@gmail.com <ritrix@gmail.com@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-02-26 13:54:38 +0000

    Commit MaskCircle

--- interpreter/source/include/MaskCircle.h
+++ interpreter/source/include/MaskCircle.h
@@ -2,6 +2,11 @@
 #ifndef __MASK_CIRCLE_H__
 #define __MASK_CIRCLE_H__
 
+#include "Mask.h"
+#include "MaskBox.h"
+#include "MaskList.h"
+#include <math.h>
+
 //! Máscara de colisión única de forma circular.
 /*!
 	Se trata de un hijo de Mask, representando la máscara de colisión
@@ -18,7 +23,7 @@ class MaskCircle : public Mask
 	int ycenter;
 	
 	//! Radio de la máscara circular.
-	float ratio;
+	float radius;
 	
 	//! Construye la máscara a partir del centro y radio del círculo y las dimensiones globales.
 	/*!
@@ -41,7 +46,29 @@ class MaskCircle : public Mask
 		\param other Máscara con la que se comprueba la colisión.
 		\return Lista de pares de colisión entre las 2 máscaras.
 	*/
-	list<CollisionPair>* collide(Mask* other);
+	vector<CollisionPair>* collide(Mask* other);
+
+	private:
+
+	//! Calcula la distancia de un punto a otro punto.
+	/*!
+		\param x1 Cordenada x del primer punto.
+		\param x2 Cordenada x del segundo punto.
+		\param y1 Cordenada y del primer punto.
+		\param y2 Cordenada y del segundo punto.
+		\return Float con el valor de la distancia entre los puntos.
+	*/
+	float getDistance (int x1, int x2, int y1, int y2);
+
+	//! Calcula si la circunferencia colisiona con los lados perpendiculares de un rectángulo.
+	/*!
+		\param bx Cordenada x del rectángulo.
+		\param by Cordenada y del rectángulo.
+		\param bwidth Ancho del rectángulo.
+		\param bheigth Alto del rectángulo.
+		\return bool true si hubo colisión, false si no hubo.
+	*/
+	bool checkBoxCollision(int bx, int by, int bwidth, int bheigth)
 };
 	
 #endif
