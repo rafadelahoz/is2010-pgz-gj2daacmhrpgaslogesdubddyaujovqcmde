5b895573cf7fe07f0cb769c78f298f6930823ba5
chrgar <chrgar@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-03-03 11:47:25 +0000

    Music, Sound y SoundEngine

--- interpreter/source/include/Sound.h
+++ interpreter/source/include/Sound.h
@@ -3,29 +3,42 @@
 #ifndef __SOUND_H__
 #define __SOUND_H__
 
+
+#include <SFML/Audio.hpp>
 #include "SoundEngine.h"
 
 using namespace std;
+
+class SoundEngine;
 
 //! Sound es la clase que envuelve el elemento del motor que representa un sonido real.
 /*!
 
 */
 class Sound {
+	friend class SoundEngine;
+	private:
+		SoundEngine* soundEngine;
+		sf::Music* sound;
+
 	public:
+		
 
 		//! Constructora parametrizada.
 		/*!
 			\param path Ruta del sonido que se desea cargar.
 			\param soundEngine El motor de sonido.
 		*/
-		Sound(string path, SoundEngine* soundEngine); 
-		
+		Sound(string path, SoundEngine* soundEngine);
+
 		//! Destructora
 		/*!
 			Libera el sonido de memoria.
 		*/
 		~Sound();
+
+		bool getLoop(){return sound->GetLoop();}
+		float getVolume(){return sound->GetVolume();}
 };
 
 #endif
