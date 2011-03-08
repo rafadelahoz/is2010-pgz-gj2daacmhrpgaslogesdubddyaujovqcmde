58bdd88ded90bad3aab8986392d43a7135f26926
pferv77 <pferv77@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-03-08 21:50:15 +0000

    +Revisados y añadidos comentarios en las clases que updateo

--- trunk/interpreter/source/src/TileMap.cpp
+++ trunk/interpreter/source/src/TileMap.cpp
@@ -1,14 +1,17 @@
 
 #include "tileMap.h"
 
+//Construtye un tileMap a partir del ancho y alto del tile y el subsistema grafico 
 TileMap::TileMap(int tileW, int tileH, GfxEngine* gfxEngine) : Graphic() 
 {
+	//Se apunta al subsistema grafico
 	this->gfxEngine = gfxEngine;
 	
 	/* Se configuran mediante setters */
 	tileSet = NULL;
 	idMap = NULL;
 	
+	//Parametros de la constructora, ancho y alto de un tile
 	this->tileW = tileW;
 	this->tileH = tileH;
 
@@ -16,28 +19,33 @@ TileMap::TileMap(int tileW, int tileH, GfxEngine* gfxEngine) : Graphic()
 	mapImage = NULL;
 };
 
+//Constructora por carga desde archivo
 void TileMap::loadMap(string fname)
 {
+	//Crea y abre un archivo de tipo un FILE a partir del nombre de archivo, del cual
+	//en el cual se encuentra el map que queremos construir
 	FILE* file = NULL;
 	file = fopen(fname.c_str(), "r");
 
-	// lectura de files y columnas de la matriz idMap
+	// lectura de filas y columnas de la matriz idMap
 	fscanf(file, "%d", &colNumber);
 	fscanf(file, "%d", &rowNumber);
 
-	// reserva memoria
+	// reserva memoria para los tiles
 	int** tiles = (int **) malloc(colNumber *sizeof(int));
 	for(int i = 0; i < colNumber; i++)
 		tiles[i] = (int *) malloc(rowNumber*sizeof(int));
 
-	// lee cada elemento de la matriz
+	//Vamos copiando el mapa de tiles en una variable auxiliar
 	for (int j = 0; j < rowNumber; j++)
 		for (int i = 0; i < colNumber; i++){
+			//lee cada elemento de la matriz
 			int aux;
 			fscanf(file, "%d", &aux);
 			tiles[i][j] = aux;
 		}
 
+	//Cerramos el FILE
 	fclose(file);
 
 	// se guarda el mapa
@@ -49,43 +57,55 @@ void TileMap::loadMap(string fname)
 		getMapImage();
 };
 
+
+//Destructora
 TileMap::~TileMap(){
 	/*
 	  Nos aseguramos de que tileSet e idMap fueron configurados 
 	  mediante el setter antes de intentar borrarlos.
 	*/
+	//Si se habia asignado un TileSet
 	if(tileSet != NULL){
+		//Lo borramos
 		delete tileSet;
 		tileSet = NULL;
 	}
-
+	//Si habiamos asignado un idMap
 	if(idMap != NULL){
+		//Por cada fila
 		for (int i = 0; i < colNumber; i++) {
+				//Borramos su contenido
 				delete idMap[i];
 				idMap[i] = NULL;
 		}
+		//Borramos el contenido de la variable
 		delete idMap;
 		idMap = NULL;
 	}
 };
 
+//Asignamos al TileMap un nuevo tileSet cargado a partir de su nombre y un ancho y alto de tile
 void TileMap::setTileSet(string tspath){
 	tileSet = new TileSet(tspath, tileW, tileH, gfxEngine);
 };
 
-
+//Asignamos al map un nuevo mapa de enteros
 void TileMap::setMap(int** map){
 	idMap = map;
 };
 
+//Asignamos el numero de columnas del TileMap
 void TileMap::setCols(int col){
 	this->colNumber = colNumber;
 };
 
+//Asignamos el número de filas del TileMap
 void TileMap::setRows(int row){
 	this->rowNumber = rowNumber;
 };
 
+
+//Devolvemos la imagen del tileMap
 Image* TileMap::getMapImage(){
 
 	// Si ya existe se devuelve la imagen ya creada.
@@ -105,6 +125,7 @@ Image* TileMap::getMapImage(){
 	return mapImage;
 };
 
+//Pinta sobre la mapImage el tile deseado del TileSet
 void TileMap::setTile(int x, int y, int tile){
 	/* 
 	dibuja sobre mapImage el tile de tileSet de coordenadas especificadas:
@@ -114,17 +135,21 @@ void TileMap::setTile(int x, int y, int tile){
 	gfxEngine->renderPart(tileSet->getImg(), x*tileW, y*tileH,
 	(tile % tileSet->getColumns())*tileW, (tile / tileSet->getColumns())*tileH, tileW, tileH, mapImage);
 };
-		
+
+	//Pinta la imagen por pantalla
 void TileMap::render(int x, int y){
-	/*Pinta el mapa en la posición indicada*/
+	//En caso de que la imagen exista
 	if (mapImage != NULL)
+		/*Pinta el mapa en la posición indicada*/
 		gfxEngine->renderExt(mapImage, x+originX, y+originY, *color, alpha, scaleH, scaleV, rotation, NULL, originX, originY);
 };
 
+//Devuelve el ancho del tileMap
 int TileMap::getWidth(){
 	return tileW;
 };
-		
+
+//Devuelve el alto del tileMap
 int TileMap::getHeight(){
 	return tileH;
 };
--- trunk/interpreter/source/src/tileSet.cpp
+++ trunk/interpreter/source/src/tileSet.cpp
@@ -1,10 +1,9 @@
 #include "TileSet.h"
 
-
 //Constructora parametrizada
 TileSet::TileSet(string path, int tileW, int tileH, GfxEngine* gfxEngine, bool transparent)
 {
-	//Escribo losa atributos de entrada
+	//Escribo los atributos de entrada
 	this->path = path;
 	this->gfxEngine = gfxEngine;
 	this->tileW = tileW;
@@ -17,29 +16,37 @@ TileSet::TileSet(string path, int tileW, int tileH, GfxEngine* gfxEngine, bool t
 	tileSetW = image->getWidth();
 	tileSetH = image->getHeigth();
 	
-	//Calculo el numero de tiles de ancho y alto que me salen para el tileSet
+	//Calculo el numero de tiles de ancho y alto que me salen para el tileSet a partir del tamaño
+	//del mismo y del alto y ancho de un tile
 	colNumber = tileSetW/tileW;
 	rowNumber = tileSetH/tileH;
 }
 
-
 //Destructora
 TileSet::~TileSet()
 {
-	delete image;
-	image = NULL;	
+	//Si habia una imagen valida en el tileSet
+	if (image != NULL)
+	{
+		//La destruimos
+		delete image;
+		image = NULL;
+	}
 }
 
+//Devuelve la imagen
 Image* TileSet::getImg()
 {
 	return image;
 }
 
+//Devuelve el número de columnas del tileSet
 int TileSet::getColumns()
 {
 	return colNumber;
 }
 
+//Devuelve el número de filas del tileSet
 int TileSet::getRows()
 {
 	return rowNumber;
