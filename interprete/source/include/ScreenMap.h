f2d9c8b7510b6a7d0158f72e8a288c72348d874d
pferv77 <pferv77>
2011-03-16 20:06:21 +0000

    +Creada la clase del interprete ScreenMap

---
+++ interprete/source/include/ScreenMap.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#ifndef __SCREENMAP_H__
+#define __SCREENMAP_H__
+
+#include "Map.h"
+#include "Entity.h"
+#include <vector>
+
+using namespace std;
+
+class ScreenMap: public Map
+{
+private:
+
+	vector<Entity*>* entidades;	//Vector de las entidades iniciales que contendrá la pantalla
+	int width;					//Ancho de la pantalla
+	int height;					//Alto de la pantalla
+	int x;						//Coordenada x absoluta del ScreenMap
+	int y;						//Coordenada y absoluta del ScreenMap
+
+public:
+
+	//Constructora parametrizada
+	ScreenMap(vector<Entity*>* entidades, int width, int heigth,int tileW, int tileH, int x, int y, GfxEngine* gfxEngine);
+	
+	//Destructora por defecto
+	~ScreenMap();
+
+	//Comprueba si la entidad que le pasan colisiona con su solidGrid
+	vector<CollisionPair>* checkColision(Entity* e);
+
+	//Comprueba si la entidad que le han pasado está dentro de los limites del mapa
+	bool isInbound(Entity* e);  
+	 
+	//Función de actualización
+	void update();
+
+	//Función de pintado
+	void render();
+}
+
+#endif __SCREENMAP_H__
---
+++ interprete/source/src/ScreenMap.cpp
@@ -0,0 +1,37 @@
+#include "ScreenMap.h"
+
+
+ScreenMap::ScreenMap(vector<Entity*>* entidades, int width, int heigth,int tileW, int tileH, int x, int y, GfxEngine* gfxEngine):Map(tileW,tileH,gfxEngine)
+{
+	//Llamo a la constructora de Map con los parametros que me pasan
+	//Pongo los atributos de la clase al valor que me dan
+	this->width = width;
+	this->height = heigth;
+	this->x = x;
+	this->y = y;
+	this->width = width;
+}
+
+ScreenMap::~ScreenMap(){};
+
+vector<CollisionPair>* ScreenMap::checkColision(Entity* e)
+{
+	//Cojo el solidGrid de mi Map, y le pido que me de el par de colision con la entidad que me han pasado
+	return this->solidGrid->collide(e->mask);
+}
+
+bool ScreenMap::isInbound(Entity* e)
+{
+	//Cojo el solidGrid de mi Map y le pregunto si la entidad se encuentra dentro del mismo
+	return this->solidGrid->isMaskInbounds(e->mask);
+}
+
+//Su funcion seria la de mantener actualizados los tiles animados, pero por ahora no hay
+void ScreenMap::update(){};
+
+//
+void ScreenMap::render()
+{
+	//Avisa al tileMap de que se renderize
+	this->tileMap->render(x,y);
+}
