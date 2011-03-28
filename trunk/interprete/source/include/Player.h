dc41c05afccffbb4b2d4a52d730f9e58e5100fd0
carloscatalanfdez <carloscatalanfdez@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-03-28 15:42:18 +0000

    + Ya se inicia el bicho cool, pero no hace nada porque el tileset del mapa no existe y la info del player tampoco. Queda retocar eso, mirar un poco a partir de change_map y move_screen y ganar.

--- trunk/interprete/source/include/Player.h
+++ trunk/interprete/source/include/Player.h
@@ -3,7 +3,7 @@
 #define __PLAYER_H__
 
 #include "GameEntity.h"
-#include "Stamp.h"
+#include "SpriteMap.h"
 #include "HelperTypes.h"
 
 class Player : public GameEntity 
@@ -12,12 +12,18 @@ class Player : public GameEntity
 private:
 
 	// info correspondiente a las tools equipdas ( <idtool, idtool>, de forma abstracta ).
-	// 
+	//
 
 public:
+
+	int hp, mp;
+
 	// crea el objeto Player, puede haber más parámetros
-	Player(int x, int y, Game* game, GameState* world, std::string gfxpath, int hp, int mp); 
+	Player(int x, int y, Game* game, GameState* world); 
     ~Player();
+
+
+	bool init(std::string gfxpath, int ncol, int nrow, int hp, int mp);
 
     // Métodos virtuales de la clase padre
     void onStep(); // Realiza el movimiento del player en función del input
