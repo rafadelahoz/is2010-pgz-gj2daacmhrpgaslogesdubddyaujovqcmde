b80f3167fe9a2c73b92abd71b24b4aba279cb5e9
Carlos Catalan <carloscatalanfdez@gmail.com>
2011-04-07 01:22:54 +0000

    + Enemy y esqueleto de component realizados.

--- interprete/source/src/Enemy.cpp
+++ interprete/source/src/Enemy.cpp
@@ -1,1 +1,92 @@
 #include "Enemy.h"
+
+
+Enemy::Enemy(int x, int y, Game* game, GameState* world, vector<Component*>* components):GameEntity(x,y,game,world)
+{
+	this->components = components;
+}
+
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::onInit()
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCInit(this);
+	}
+}
+
+void Enemy::onStep()
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCStep(this);
+	}
+}
+
+void Enemy::onRender()
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCRender(this);
+	}
+}
+
+void Enemy::onTimer(int timer)
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCTimer(this, timer);
+	}
+}
+
+void Enemy::onCollision(CollisionPair other, Entity* e)
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCCollision(this, other, e);
+	}
+}
+
+void Enemy::onDestroy()
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCDestroy(this);
+	}
+}
+
+void Enemy::onCustomEvent(int event)
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCCustomEvent(this, event);
+	}
+}
+
+void Enemy::onInitStep()
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCInitStep(this);
+	}
+}
+
+void Enemy::onEndStep()
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCEndStep(this);
+	}
+}
+
+void Enemy::onEndWorld()
+{
+	for (vector<Component*>::iterator it = components->begin(); it != components->end(); ++it) 
+	{
+		(*it)->onCEndWorld(this);
+	}
+}
