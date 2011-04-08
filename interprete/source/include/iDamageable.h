698cf8069a9ad2e9891f257814a65fac2299dcf0
strauzen@gmail.com <strauzen@gmail.com@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-04-08 10:45:20 +0000

    Creada y completada la interfaz damageable

--- interprete/source/include/iDamageable.h
+++ interprete/source/include/iDamageable.h
@@ -6,6 +6,21 @@
 class iDamageable
 {
 	public:
+		enum DamageTypes { PHYSICAL, MAGIC, PROJECTILE, FIRE, ICE, EARTH, ELECTRIC, DEATH };
+		
+		int hp;
+		int maxHp;
+		int defense;
+		short typeWeakness;
+
+		iDamageable();
+		iDamageable(int hp, int maxHp, int defense, short typeWeakness);
+		~iDamageable();
+
+		virtual void onDeath();
+
+		void init(int hp, int maxHp, int defense, short typeWeakness);
+		void onDamage(int damage, short damageType);
 };
 
 #endif
---
+++ interprete/source/src/iDamageable.cpp
@@ -0,0 +1,37 @@
+#include "iDamageable.h"
+
+//Inicializa los valores por defecto, vida 2 defensa 0 debilidad todo.
+iDamageable::iDamageable()
+{
+	init(2, 2, 0, 0xf);
+}
+
+iDamageable::iDamageable(int hp, int maxHp, int defense, short typeWeakness)
+{
+	init(hp,maxHp,defense,typeWeakness);
+}
+
+void iDamageable::init(int hp, int maxHp, int defense, short typeWeakness)
+{
+	this->hp = hp;
+	this->maxHp = maxHp;
+	this->defense = defense;
+	this->typeWeakness = typeWeakness;
+}
+
+/*Comprueba si la entidad que implementa la interfaz es debil a ese tipo de daño y en caso
+  afirmativo realiza el daño, a continuación si la vida es <= 0 llama a onDeath y pone la 
+  vida a 0*/
+void iDamageable::onDamage(int damage, short damageType)
+{
+	short aux = typeWeakness & damageType;
+	if (aux != 0x0)
+	{
+		hp = damage - defense;
+		if (hp <= 0)
+		{
+			hp = 0;
+			onDeath();
+		}
+	}
+}
