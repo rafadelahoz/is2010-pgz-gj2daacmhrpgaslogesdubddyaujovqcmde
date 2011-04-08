66a3b084a35c9e38f5ab28cb99d222b2e59f9f7b
Rafa de la Hoz <rafadelahoz@users.noreply.github.com>
2011-04-08 11:58:10 +0000

    Añadidas a player las primeras pinceladas del sistema de animaciones.

--- interprete/source/include/Player.h
+++ interprete/source/include/Player.h
@@ -14,15 +14,9 @@ class Controller;
 
 class Player : public GameEntity 
 {
-
-private:
-
-	// info correspondiente a las tools equipdas ( <idtool, idtool>, de forma abstracta ).
-	Controller* controller;
-
-	bool getNewPos(int& xtemp, int& ytemp);
-
 public:
+
+	enum PlayerAnim { Stand, Walk, Slash, Thrust, Hit, Grab, Pull, Push, Pick, Picked, Throw, Drop };
 
 	int hp, mp;
 
@@ -42,6 +36,45 @@ public:
 
     // Métodos propios
 	Dir getDir();
+
+	// Hace al player reproducir la animación indicada
+	// Cuando termine volverá al estado anterior al comienzo
+	// Devuelve true si el player acepta ser manipulado
+	bool playAnim(PlayerAnim anim);
+
+private:
+
+	// info correspondiente a las tools equipdas ( <idtool, idtool>, de forma abstracta ).
+	Controller* controller;
+
+	// Animation translator from PlayerAnim to anim name
+	std::map<PlayerAnim, std::string> animList;
+	std::string getAnimName(PlayerAnim anim);
+
+	struct PlayerMask
+	{
+		int offsetX; int offsetY;
+		int width; int height;
+	};
+
+	struct PlayerFrameData
+	{
+		PlayerMask walkMask;
+		PlayerMask collisionMask;
+		int hotspotX, hotspotY;
+	};
+
+	struct PlayerAnimData
+	{
+		int animSpeed;
+		unsigned char numFrames;
+		std::vector<PlayerFrameData> frameData;
+	};
+
+	// List of animation data
+	std::map<PlayerAnim, PlayerAnimData> animData;
+
+	bool getNewPos(int& xtemp, int& ytemp);
 };
 
 #endif
