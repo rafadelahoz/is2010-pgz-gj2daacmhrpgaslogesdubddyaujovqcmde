7f33b342bb9b323a900d9ecf09cc53a06c538da5
Gloria <glores@users.noreply.github.com>
2011-04-22 10:41:20 +0000

    Los NPCs ahora cargan sus animaciones desde un archivo de configuración. Apañado también el fallito de que los NPCs andaban contra una pared como si estuvieran locos (lo cual quedaba muy bien para el texto de "Ese está loco")

--- interprete/source/include/NPC.h
+++ interprete/source/include/NPC.h
@@ -10,11 +10,55 @@ class NPC : public GameEntity, public iInteractable {
 	private:
 
 		enum State {move, idle};
+		enum NPCAnim {Stand, Walk};
 
 		Direction dir;
 		int ox, oy;
 		int sp;
 		State state;
+
+		struct NPCMask {
+			int offsetX; int offsetY;
+			int width; int height;
+		};
+
+		struct NPCFrameData {
+			int frameId;
+			int speed;
+			NPCMask walkMask;
+			NPCMask collisionMask;
+		};
+
+		struct NPCAnimData {
+			int animSpeed;
+			unsigned char numFrames;
+			std::vector<NPCFrameData> frameData;
+		};
+
+		// Animation translator from PlayerAnim to anim name
+		std::map<std::pair<NPCAnim, Direction>, std::string> animList;
+		std::string getAnimName(NPCAnim anim, Direction dir);
+
+		// List of animation data
+		std::map<std::pair<NPCAnim, Direction>, NPCAnimData> animDataList;
+		NPCAnimData getAnimationData(NPCAnim anim, Direction dir = NONE);
+
+		// --------------------- Métodos de auxiliares ----------------------------
+
+		bool playAnim(NPCAnim anim, int speed = -1, Direction dir = NONE);
+
+		/* Carga una animación de la herramienta (si es una herramienta animada) */
+		bool loadAnimation(NPCAnim anim,Direction dir, std::string name, FILE* from);
+
+		/* Coge el identificador de un frame del archivo de configuración */
+		NPCFrameData loadAnimationFrame(FILE* from);
+
+		/* Devuelve el nombre del fichero de configuración de una imagen */
+		std::string getConfigurationFileName(std::string fname);
+
+		/* Carga todas las animaciones de una imagen */
+		bool loadAnimations(std::string fname);
+
 	public:
 
 		NPC(int x, int y, Game* game, GameState* world);
--- interprete/source/src/NPC.cpp
+++ interprete/source/src/NPC.cpp
@@ -6,7 +6,10 @@ NPC::NPC(int x, int y, Game* game, GameState* world) : GameEntity(x, y, game, wo
 	type = "npc";
 }
 
-NPC::~NPC() {}
+NPC::~NPC() {
+	animList.clear();
+	animDataList.clear();
+}
 
 void NPC::init(string graphicpath, int ncol, int nrow) {
 	graphic = new SpriteMap(graphicpath, ncol, nrow, game->getGfxEngine());
@@ -17,39 +20,7 @@ void NPC::init(string graphicpath, int ncol, int nrow) {
 	dir = DOWN;
 	this->setTimer(0, 60); 
 
-	vector<int>* frames = new vector<int>();
-	frames->push_back(0);
-	frames->push_back(1);
-	frames->push_back(0);
-	frames->push_back(2);
-	((SpriteMap*) graphic)->addAnim("up", frames, 3, true);
-
-	frames = new vector<int>();
-	frames->push_back(3);
-	frames->push_back(4);
-	frames->push_back(3);
-	frames->push_back(5);
-	((SpriteMap*) graphic)->addAnim("down", frames, 4, true);
-
-	frames = new vector<int>();
-	frames->push_back(6);
-	frames->push_back(7);
-	frames->push_back(6);
-	frames->push_back(8);
-	((SpriteMap*) graphic)->addAnim("right", frames, 4, true);
-
-	frames = new vector<int>();
-	frames->push_back(9);
-	frames->push_back(10);
-	frames->push_back(9);
-	frames->push_back(11);
-	((SpriteMap*) graphic)->addAnim("left", frames, 4, true);
-
-	frames = new vector<int>();
-	frames->push_back(3);
-	((SpriteMap*) graphic)->addAnim("idle", frames, 4, true);
-
-	((SpriteMap*) graphic)->playAnim("idle");
+	loadAnimations(getConfigurationFileName(graphicpath));
 }
 
 void NPC::onStep(){
@@ -61,12 +32,16 @@ void NPC::onStep(){
 						ox = x;
 						oy = y;
 						state = idle;
-						((SpriteMap*) graphic)->playAnim("idle");
+						((SpriteMap*) graphic)->playAnim(getAnimName(Stand, dir));
 					}
 					else{
 						y -= sp;
-						((SpriteMap*) graphic)->playAnim("up");
+						((SpriteMap*) graphic)->playAnim(getAnimName(Walk, dir));
 					}
+				}
+				else{
+					state = idle;
+					((SpriteMap*) graphic)->playAnim(getAnimName(Stand, dir));
 				}
 				break;
 			case DOWN:
@@ -75,12 +50,16 @@ void NPC::onStep(){
 						ox = x;
 						oy = y;
 						state = idle;
-						((SpriteMap*) graphic)->playAnim("idle");
+						((SpriteMap*) graphic)->playAnim(getAnimName(Stand, dir));
 					}
 					else{
 						y += sp;
-						((SpriteMap*) graphic)->playAnim("down");
+						((SpriteMap*) graphic)->playAnim(getAnimName(Walk, dir));
 					}
+				}
+				else{
+					state = idle;
+					((SpriteMap*) graphic)->playAnim(getAnimName(Stand, dir));
 				}
 				break;
 			case LEFT:
@@ -89,12 +68,16 @@ void NPC::onStep(){
 						ox = x;
 						oy = y;
 						state = idle;
-						((SpriteMap*) graphic)->playAnim("idle");
+						((SpriteMap*) graphic)->playAnim(getAnimName(Stand, dir));
 					}
 					else{
 						x -= sp;
-						((SpriteMap*) graphic)->playAnim("left");
+						((SpriteMap*) graphic)->playAnim(getAnimName(Walk, dir));
 					}
+				}
+				else{
+					state = idle;
+					((SpriteMap*) graphic)->playAnim(getAnimName(Stand, dir));
 				}
 				break;
 			case RIGHT:
@@ -103,12 +86,16 @@ void NPC::onStep(){
 						ox = x;
 						oy = y;
 						state = idle;
-						((SpriteMap*) graphic)->playAnim("idle");
+						((SpriteMap*) graphic)->playAnim(getAnimName(Stand, dir));
 					}
 					else{
 						x += sp;
-						((SpriteMap*) graphic)->playAnim("right");
+						((SpriteMap*) graphic)->playAnim(getAnimName(Walk, dir));
 					}
+				}
+				else{
+					state = idle;
+					((SpriteMap*) graphic)->playAnim(getAnimName(Stand, dir));
 				}
 				break;
 		}
@@ -150,14 +137,179 @@ void NPC::onTimer(int timer){
 	this->setTimer(0, rand()%60);
 }
 
-void NPC::onInteract(Player* p)
-{
-	switch (p->dir)
-	{
-	case DOWN: dir = UP; break;
-	case UP: dir = DOWN; break;
-	case LEFT: dir = RIGHT; break;
-	case RIGHT: dir = LEFT; break;
+void NPC::onInteract(Player* p) {
+	switch (p->dir)	{
+		case DOWN: dir = UP; break;
+		case UP: dir = DOWN; break;
+		case LEFT: dir = RIGHT; break;
+		case RIGHT: dir = LEFT; break;
 	}
 	p->playAnim(Player::Walk, -1, DOWN);
+}
+
+std::string NPC::getConfigurationFileName(string fname) {
+	// El fname debe tener extensión
+	if (fname.length() < 5)
+		return "";
+
+	std::string cfgname = fname;
+	// Se quita la extensión del fname (.png)
+	for (int i = 0; i < 3; i++)
+		cfgname.pop_back();
+	cfgname.append("cfg");
+
+	return cfgname;
+}
+
+bool NPC::loadAnimations(string fname) {
+	SpriteMap* gfx = ((SpriteMap*) graphic);
+	int nCols = 0, nRows = 0;
+
+	// Carga el archivo de config y lee
+	FILE* f = fopen(fname.c_str(), "r");
+
+	// Si el archivo es inválido, no se puede hacer nada
+	if (f == NULL)
+		return false;
+
+	// 1. Número de columnas y filas del sprite sheet
+	if (fscanf(f, "%d %d", &nCols, &nRows) < 2)
+		return false;
+
+	// 2. Leer las animaciones
+	// Stand
+	loadAnimation(Stand, UP, "idleUp", f);
+	loadAnimation(Stand, DOWN, "idleDown", f);
+	loadAnimation(Stand, RIGHT, "idleRight", f);
+	loadAnimation(Stand, LEFT, "idleLeft", f);
+
+	// Walk
+	loadAnimation(Walk, UP, "up", f);
+	loadAnimation(Walk, DOWN, "down", f);
+	loadAnimation(Walk, RIGHT, "right", f);
+	loadAnimation(Walk, LEFT, "left", f);
+
+	fclose(f);
+
+	return true;
+}
+
+bool NPC::loadAnimation(NPCAnim anim, Direction dir, std::string name, FILE* from) {
+	// Cargar animación indicada de from
+	if (from == NULL || name == "")
+		return false;
+
+	int numFrames, speed;
+	int* frameList;
+	NPCAnimData animData;
+	NPCFrameData frameData;
+
+	// Leemos datos de la animación
+
+	// 0.Speed
+	if (fscanf(from, "%d", &speed) < 1)
+		return false;
+
+	// 1.Nº frames
+	if (fscanf(from, "%d", &numFrames) < 1)
+		return false;
+
+	animData.animSpeed = speed;
+	animData.numFrames = numFrames;
+	//animData.dir = dir;
+
+	// Se instancia el contenedor de frames
+	frameList = new int[numFrames];
+
+	// Se carga cada frame
+	for (int i = 0; i < numFrames; i++) 	{
+		frameData = loadAnimationFrame(from);
+		frameList[i] = frameData.frameId;
+		animData.frameData.push_back(frameData);
+	}
+
+	// Se añade la animación al graphic con el nombre indicado
+	SpriteMap* gfx = ((SpriteMap*) graphic);
+	gfx->addAnim(name, frameList, numFrames, speed, true);
+
+	// Se mappea la anim
+	animList.insert(make_pair(make_pair(anim, dir), name));
+	// Y se agrega su info
+	animDataList.insert(make_pair(make_pair(anim, dir), animData));
+	delete frameList;
+
+	return true;
+}
+
+NPC::NPCFrameData NPC::loadAnimationFrame(FILE* from) {
+	// Se carga un frame de from
+	// Se prepara un frame vacío
+	NPCFrameData fd;
+	fd.frameId = 0;
+	fd.walkMask.offsetX = 0;
+	fd.walkMask.offsetY = 0;
+	fd.walkMask.width = 0;
+	fd.walkMask.height = 0;
+	fd.collisionMask.offsetX = 0;
+	fd.collisionMask.offsetY = 0;
+	fd.collisionMask.width = 0;
+	fd.collisionMask.height = 0;
+
+	// El archivo debe ser válido
+	if (from == NULL)
+		return fd;
+
+	// Se lee el frameId
+	if (fscanf(from, "%d", &fd.frameId) < 1)
+		return fd;
+
+	// Y por ahora ya
+	return fd;
+}
+
+NPC::NPCAnimData NPC::getAnimationData(NPCAnim anim, Direction dir) {
+	if (dir == NONE) dir = this->dir;
+	// Se obtiene el nombre de la animación a partir del enum
+	std::map<std::pair<NPCAnim, Direction>, NPCAnimData>::iterator it;
+	it = animDataList.find(make_pair(anim, dir));
+	// Si el iterador alcanca el final de la lista, no está la anim
+	if (it == animDataList.end()) 	{
+		NPCAnimData a; a.animSpeed = -1; a.numFrames = -1;
+		return a;
+	}
+	else
+		return (*it).second;
+}
+
+bool NPC::playAnim(NPCAnim anim, int speed, Direction dir) {
+	if (dir == NONE)
+		dir = this->dir;
+	// Si la animación no existe, seguro que no se puede
+	std::string name = getAnimName(anim, dir);
+	if (name == "")
+		return false;
+
+	// Si la animación no tiene datos, algo va mal
+	NPCAnimData data = getAnimationData(anim, dir);
+	if (data.numFrames < 0)
+		return false;
+
+	if (speed < 0)
+		speed = data.animSpeed;
+
+	((SpriteMap*) graphic)->playAnim(name, speed, false, false);
+
+	return true;
+}
+
+std::string NPC::getAnimName(NPCAnim anim, Direction dir) {
+	if (dir == NONE) dir = this->dir;
+	// Se obtiene el nombre de la animación a partir del enum
+	std::map<std::pair<NPCAnim, Direction>, std::string>::iterator it;
+	it = animList.find(make_pair(anim, dir));
+	// Si el iterador alcanca el final de la lista, no está la anim
+	if (it == animList.end())
+		return "";
+	else
+		return (*it).second;
 }
