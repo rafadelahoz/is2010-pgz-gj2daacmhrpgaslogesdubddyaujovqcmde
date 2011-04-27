7c6e9d697dc3286d309727aed2b36f8f2ca70a18
carloscatalanfdez <carloscatalanfdez@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-04-27 13:59:32 +0000

    + Leaks de memoria en menu solved (grafic del cursor y doble controller al iniciar el juego) + Teleporter ahora puede transportar pisando o estando dentro.

--- trunk/interprete/source/src/Teleporter.cpp
+++ trunk/interprete/source/src/Teleporter.cpp
@@ -3,11 +3,13 @@
 
 Teleporter::Teleporter(MapLocation m, int x, int y, Game* g, GameState* gs, int w, int h) : Entity(x, y, g, gs)
 {
-	mask = new MaskBox(0, 0, w, h, "teleporter");
+	mask = new MaskBox(x, y, w, h, "teleporter");
 	graphic = new Stamp("data/graphics/coltest.png", g->getGfxEngine());
 	solid = false;
 
 	destination = m;
+	teleportType = STEP;
+	transitionEffect = FADE;
 }
 
 Teleporter::~Teleporter()
@@ -18,6 +20,42 @@ void Teleporter::onCollision(CollisionPair pair, Entity* other)
 {
 	if (pair.b == "player")
 	{
-		((PGZGame*) game)->controller->teleportTo(destination, (Player*) other, FADE, false);
+		if (teleportType == STEP)
+		{
+			((PGZGame*) game)->controller->teleportTo(destination, (Player*) other, transitionEffect, false);
+		}
+		else if (teleportType == INSIDE)
+		{
+			if (isInside(other->mask))
+			{
+				((PGZGame*) game)->controller->teleportTo(destination, (Player*) other, transitionEffect, false);
+			}
+		}
 	}
+}
+
+void Teleporter::setTeleportType(TeleportType type)
+{
+	teleportType = type;
+}
+
+void Teleporter::setTransition(TransitionEffect te)
+{
+	transitionEffect = te;
+}
+
+bool Teleporter::isInside(Mask* mask)
+{
+	//Calculamos los vertices superior izquierdo e inferior derecho
+	// Vértice superior izquierdo
+	int x1 = mask->x + mask->xoffset;
+	int y1 = mask->y + mask->yoffset;
+	// Vértice inferior derecho
+	int x2 = mask->x + mask->xoffset + mask->width;
+	int y2 = mask->y + mask->yoffset + mask->height;
+
+	if ((x1 >= this->mask->x + mask->xoffset) && (x2 <= this->mask->x + mask->xoffset + this->mask->width) && (y1 >= this->mask->y + mask->yoffset) && (y2 <= this->mask->y + mask->yoffset + this->mask->height))
+		return true;
+
+	return false;
 }
