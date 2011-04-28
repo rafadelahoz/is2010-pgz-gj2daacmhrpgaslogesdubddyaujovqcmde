1a6a4c93803ec26256d6bd1fc72821c1db83c9a9
pferv77 <pferv77>
2011-04-28 22:14:12 +0000

    + Ahora los bloqueos no están parados, tienen animación tanto de quieto como al morir, lo que implica su propia imagen y archivo de configuración
    + He añadido una hierba fea que me he bajado,las animaciones las he hecho yo, por si os lloran los ojos.

--- interprete/source/src/DamageableBlockade.cpp
+++ interprete/source/src/DamageableBlockade.cpp
@@ -5,25 +5,119 @@ DamageableBlockade::DamageableBlockade(int x, int y, Game* game, GameState* game
 {
 }
 
-void DamageableBlockade::init(short typeWeakness, Graphic* graphic, int xColision, int yColision)
+void DamageableBlockade::init(short typeWeakness, string gfxPath, int xColision, int yColision)
 {
 	iDamageable::init(1,1,0,typeWeakness);
-	this->graphic = graphic;
+	loadAnimations(gfxPath);
 	this->mask = new MaskBox(x, y, xColision, yColision, "DamageableBlockade");
 	this->solid = true;
+	((SpriteMap*) graphic)->playAnim("stand");
 }
 
-
 /*/////////////////////////////////////////////////////////////////////////////////////////
-Hay que cambiar el graphic que le pasan por una ruta para que yo lo cargue, habrá un spriteSheet
-y un archivo de configuración con ancho de tile y alto de tile, xoffset y yoffset, columnas y filas
+El archivo de configuración contiene columnas y filas, velocidad y numero de frames
 y luego 4 enteros que serán la animación de parado y otros 4 de la animacion de muerte
 /////////////////////////////////////////////////////////////////////////////////////////*/
 
+bool DamageableBlockade::loadAnimations(string fname)
+{
+	// se busca extensión
+    size_t found = fname.find_last_of(".");
+        
+    //Si la encuentra
+    if (found != string::npos){
+            //Se descarta 
+            fname = fname.substr(0,found);
+    }
+	
+	// Se almacena el nombre de archivo sin extensión
+    string fnameConf = fname;
+
+    // carga configuracion y tileSet de los ficheros correspondientes
+    //nombreArchivo.png
+    fname = fname.append(".png");
+    //nombreArchivo.txt
+    fnameConf= fnameConf.append(".cfg");
+	
+	// Carga el archivo de config y lee
+	FILE* f = fopen(fnameConf.c_str(), "r");
+
+	// Si el archivo es inválido, no se puede hacer nada
+	if (f == NULL)
+		return false;
+
+	// 1. Ancho y alto de imagen 
+	int ncol = 0, nrow = 0;
+	if (fscanf(f, "%d %d", &ncol, &nrow) < 2)
+		return false;
+
+	//Una vez tengo ya el ancho y el alto puedo cargar el SpriteMap que será mi grafico
+	graphic = new SpriteMap(fname, ncol, nrow, game->getGfxEngine());
+	
+	// Leo todas las animaciones
+	// Stand
+	loadAnimation(Stand, "stand", f);
+
+	//Death
+	loadAnimation(Death, "death", f);
+
+	//Cierro el archivo y salgo
+	fclose(f);
+
+	return true;
+};
+
+
+bool DamageableBlockade::loadAnimation(BlockadeAnim anim, string name, FILE* file)
+{
+	// Cargar animación indicada de from
+	if (file == NULL || name == "")
+		return false;
+
+	int numFrames, speed, auxFrame;
+	int* frameList;
+
+	// Leemos datos de la animación
+
+	// 0.Speed
+	if (fscanf(file, "%d", &speed) < 1)
+		return false;
+
+	// 1.Nº frames
+	if (fscanf(file, "%d", &numFrames) < 1)
+		return false;
+
+	// Se instancia el contenedor de frames
+	frameList = new int[numFrames];
+
+	// Se carga cada frame
+	for (int i = 0; i < numFrames; i++)
+	{
+		if (fscanf(file, "%d", &auxFrame) < 1)
+			return false;
+		frameList[i] = auxFrame;
+	}
+
+	// Se añade la animación al graphic con el nombre indicado
+	SpriteMap* spMap = ((SpriteMap*) graphic);
+	spMap->addAnim(name, frameList, numFrames, speed, true);
+
+	delete frameList;
+
+	return true;
+}
 
 void DamageableBlockade::onDeath()
 {
-	instance_destroy();
+	((SpriteMap*) graphic)->playAnim("death",(float) 1,false);
+	setTimer(2,32);
+}
+
+//esto está temporal, la muerte deberia ser al final de animación
+void DamageableBlockade::onTimer(int n)
+{
+	if (n == 2)
+		instance_destroy();
 }
 
 //esto está temporal, es el arma el que deberia llamar a la onDamage
