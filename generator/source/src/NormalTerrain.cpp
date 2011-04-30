bb4f55cfa95154c96e6d9658b6e4f433d6224752
apsantorio <apsantorio@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-04-30 19:02:21 +0000

    + Autotile de AutoTerrain finiquitao (en teoría)

--- generator/source/src/NormalTerrain.cpp
+++ generator/source/src/NormalTerrain.cpp
@@ -11,15 +11,17 @@ NormalTerrain::~NormalTerrain()
 }
 
 // i filas, j columnas
-void NormalTerrain::toTiles(int** source, Screen* screen, int w, int h, int i, int j)
+void NormalTerrain::toTiles(int** source, int** screen, int w, int h, int i, int j)
 {
-	if (i < w && i >= 0 && j < h && j >= 0 && screen != NULL)
-	{
+	int row = 2*i;
+	int col = 2*j;
+
+	if (i < w && i >= 0 && j < h && j >= 0 && screen != NULL){
 		// simplemente pone su tile gráfico en la posición indicada de la pantalla
-		screen->setTile(i, j, tile);
-		screen->setTile(i+1, j, tile+1);
-		screen->setTile(i, j+1, tile+chipsetW);
-		screen->setTile(i+1, j+1, tile+chipsetW+1);
+		screen[row][col] = tile;
+		screen[row][col+1] = tile + 1;
+		screen[row+1][col] = tile + chipsetW;
+		screen[row+1][col+1] = tile + chipsetW + 1;
 	}
 }
 
