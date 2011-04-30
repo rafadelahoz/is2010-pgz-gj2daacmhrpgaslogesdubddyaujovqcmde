00c91540f7a8dc5a547bef787249cfd20d41a7b9
javieku <javieku@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-04-30 10:12:34 +0000

    + Nueva versión de genPuzzle algo verde todavía. + Subo a dropbox en iteraciones - q2i7 - mapas un ejemplo de dungeonJ y de Overworld no hay manera de subir al repositorio los 335 archivos de mapas. (tras 4 intentos da un fallo bad gateway(?) al enviar el contenido)

--- trunk/generator/source/src/GenPuzzle.cpp
+++ trunk/generator/source/src/GenPuzzle.cpp
@@ -1,31 +1,66 @@
 #include "GenPuzzle.h"
 
-GenPuzzle::GenPuzzle(short type) {
-	puzzle = new puzzle_t;
+const void* GenPuzzle::genPuzzle = NULL;
 
-	puzzle->type = type;
+void* GenPuzzle::operator new (size_t size){
+	if(genPuzzle == NULL){
+		void* p = malloc(size);
+		return p;
+	}
+	else
+		return (void*)genPuzzle;
+}
+
+GenPuzzle::GenPuzzle(short item, string zone, DBManager* db) {
+	if(genPuzzle == NULL){
+		genPuzzle = this;
+		this->item = item;
+		this->zone = zone;
+		this->db = db;
+	}
 };
 
-GenPuzzle::~GenPuzzle() {
-	delete puzzle;
-};
+GenPuzzle::~GenPuzzle() {};
 
-void GenPuzzle::generate(DunScreen* ds, short id) {
-	
-	puzzle->id = id;
+void GenPuzzle::generate(DunScreen* ds, short id, short type) {
 
-	switch(puzzle->type){
+	switch(type){
 		case(0):
 			enemyArena(ds);
 			break;
 	}
-
-	// Añade al vector de puzzles tipo e identificación tal vez debería ser lista enlazada en vez de vector?
-	ds->setPuzzle(puzzle);
 }
 
-void GenPuzzle::enemyArena(DunScreen* ds) {
-	// Coloca enemigosds->setEnemies(); blabla
-	// Añadir entidad resultante al generar puzzle puzzle->setEntity();
+void GenPuzzle::enemyArena(DunScreen* ds, int id) {
+	short e = -1;
+	short n = rand() % 5 + 5;
+	// Pide un enemigo válido a la interfaz con la base de datos
+	e = db->getEnemy(zone, theme);
+    if(e != -1)
+		for (int i = 0; i < n; i++) {
+			// Escoge una localización válida en la habitación
+			short x, y, s;
+			do {
+				x = (rand() % (SCREEN_WIDTH - ds->getWall_size()*2)) + ds->getWall_size();
+				y = (rand() % (SCREEN_HEIGHT - ds->getWall_size()*2)) + ds->getWall_size();
+				s = ds->getSolid(x,y);
+			} while (s != 0 || ds->place_free(x, y));
+			// Al salir del bucle, x e y representan una posición válida para el enemigo e
+			enemy en;
+			en.id = e;
+			en.posX = x;
+			en.posY = y;
+
+			ds->addEnemy(en);
+		}
+	/* type x y<--sin sentido en este contexto idCollectable linkedto variable */
+	ds->addEntity(new doorCloser(DOOR_CLOSER,x,y,id+1,id,-1));
+
+	ds->addEntity(new EntityArena(ARENA,x,y,id,-1));
+
+	if(item != -1)
+		entities->push_back(new EntityItem(ITEM,x,y,id+2,id,-1,-1,-1));
+
+	ds->addEntity(new doorOpener(DOOR_OPENER,-1,id,-1,));
 }
 
