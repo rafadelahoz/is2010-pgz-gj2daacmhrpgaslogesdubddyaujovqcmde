12631852263d2ea2afdb4e61ca20fa6f08e22287
javieku <javieku@users.noreply.github.com>
2011-04-30 10:12:34 +0000

    + Nueva versión de genPuzzle algo verde todavía. + Subo a dropbox en iteraciones - q2i7 - mapas un ejemplo de dungeonJ y de Overworld no hay manera de subir al repositorio los 335 archivos de mapas. (tras 4 intentos da un fallo bad gateway(?) al enviar el contenido)

--- generator/source/include/GenPuzzle.h
+++ generator/source/include/GenPuzzle.h
@@ -3,20 +3,44 @@
 #ifndef _GENPUZZLE_H_
 #define _GENPUZZLE_H_
 
+#include <new>
+
 #include "Screen.h"
 #include "DunScreen.h"
 
 class GenPuzzle{
+	 /*
+		El puzzle se modela como un conjunto de entidades, tanto los simples como los anidados, que volcamos 
+		al vector entities de ds durante el metodo correspondiente en generate. 
+		Se mezclaría con los demás porque he supuesto que linkedto
+		se utilizaría para establecer la precedencia temporal entre las entidades, vamos que doorCloser y dooropener están linkedto arena y
+		en función de su estado actuan sin problemas con otras entidades que pueda haber en ese vector(?).
+		Probablemente es prescindible el vector de puzzles, porque el identificador también lo llevan las entidades o eso he visto en el caso 
+		de las puertas, además el tipo ya no tiene sentido.
+
+		También he añadido singleton. Aunque agregé las entidades todavía en Entity.h
+	 */
+
 	private:
-		puzzle_t* puzzle;
+
+		const static void* genPuzzle; // Controlamos la instanciación de la clase con este puntero.
+
+		short item; // elemento que resulta tras resolver el puzzle.
+		string zone; // Zona en la que se encuentra la mazmorra.
+		DBManager* db;
 
 	public:
-		GenPuzzle(short type);
+
+		GenPuzzle(DBManager* db);
 		~GenPuzzle();
 
-		void generate(DunScreen* ds, short id);
+		// Sobrecarga de new para controlar la reserva de memoria
+		static void* operator new (size_t size);
 
-		// genera el puzzle arena sobre ds
+		// Genera el puzzle sobre ds con identificador id y tipo type
+		void generate(DunScreen* ds, short id, short type);
+
+		// Genera el puzzle arena sobre ds
 		void enemyArena(DunScreen* ds); 
 };
 
