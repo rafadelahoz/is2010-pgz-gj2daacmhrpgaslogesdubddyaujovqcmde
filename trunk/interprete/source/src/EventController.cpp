6c99e472b1b55341aaec306cfeecb79de38de8e0
gepozuelo <gepozuelo@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-05-03 14:56:04 +0000

    + Añadidos los config para la herramienta bastón mágico. De momento son asquerosos, lo sé. Intentaré spritearlos en un futuro cercano.

--- trunk/interprete/source/src/EventController.cpp
+++ trunk/interprete/source/src/EventController.cpp
@@ -160,12 +160,12 @@ void EventController::onStep()
 
 				if (game->getInput()->keyPressed(Input::kG))
 				{
-					controller->toolController->equip(1,controller->getPlayer(0), 0);
+					controller->toolController->equip(1, controller->getPlayer(0), 0);
 				}
 
 				else if (game->getInput()->key(Input::kF))
 				{
-					controller->toolController->equip(3,controller->getPlayer(0), 1);
+					controller->toolController->equip(3, controller->getPlayer(0), 1);
 				}
 
 				if (game->getInput()->keyPressed(Input::kU))
