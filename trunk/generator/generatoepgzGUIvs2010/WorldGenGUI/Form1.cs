c7bbeb977e0090c5bd0afbb19c405091220b8d14
ritrix@gmail.com <ritrix@gmail.com@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-05-04 22:20:15 +0000

    + Subiendo GeneratorGUI con las mejoras de esta mañana.

--- trunk/generator/generatoepgzGUIvs2010/WorldGenGUI/Form1.cs
+++ trunk/generator/generatoepgzGUIvs2010/WorldGenGUI/Form1.cs
@@ -11,9 +11,54 @@ namespace WorldGenGUI
 {
     public partial class Form1 : Form
     {
+        private ComboBoxEx comboSelecChar = new ComboBoxEx();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboSelecChar.ImageList = charImgList;
+            comboSelecChar.DropDownStyle = ComboBoxStyle.DropDownList;
+            // just pass these in instead of strings, class included below
+            // specify a valid imageIndex
+            comboSelecChar.Items.Add(new ComboBoxExItem("Indi Faker", 0));
+            comboSelecChar.Items.Add(new ComboBoxExItem("Skull Man", 1));
+            comboSelecChar.Location = new Point(225, 40);
+            tabPage1.Controls.Add(comboSelecChar);
+            comboSelecChar.Height = charImgList.ImageSize.Height * charImgList.Images.Count;
+            comboSelecChar.ItemHeight = charImgList.ImageSize.Height;
+
+            chkListBoxTools.SetItemChecked(3, true);
+        }
+
+        private String genName(){
+            Random rand = new Random();
+            string name = "";
+            int namelen = rand.Next(4, 10);
+
+            string[] letters = new string[2] { "bcdfghjklmnpqrstvwxyz", "aeiou" };
+            int[] letterlen = new int[2] { letters[0].Length, letters[1].Length };
+
+            for (int i = 0; i < namelen; i++)
+                name += letters[i % 2][rand.Next() % letterlen[i % 2]];
+
+            string capitalName = "";
+            capitalName += char.ToUpper(name[0]);
+            capitalName += name.Substring(1);
+            return capitalName;
+        }
+
+        private void butGenerate_Click_1(object sender, EventArgs e)
+        {
+            tBoxName.Text = genName();
+        }
+
+        private void butClose_Click_1(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
