a0240cd5b6a16f05a7f5cd051462975c3213efa7
apsantorio <apsantorio@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-05-10 18:20:02 +0000

    + Modificado tileEntity para que tenga varios tiles + modificado toEntities en Decorator

--- trunk/generator/source/src/Decoration.cpp
+++ trunk/generator/source/src/Decoration.cpp
@@ -17,34 +17,39 @@ void Decoration::init(int x, int y)
 	this->y = y;
 };
 
-std::vector<Entity*> Decoration::toEntities()
+Entity* Decoration::toEntities()
 {
-	std::vector<Entity*> v;
 	std::vector<int>::iterator itTile, itType;
-	short solid;
 
 	if (data.tiles.size() != data.tileTypes.size())
-		return v;	// error al cargar no coinciden el nº de tiles con sus tipos
+		return NULL;	// error al cargar no coinciden el nº de tiles con sus tipos
+
+	// creamos los vectores para la entidad tileEntity
+	short* tiles = new short[data.tiles.size()];
+	short* tilesType = new short[data.tileTypes.size()];
 
 	// iniciamos los iteradores
 	itTile = data.tiles.begin();
 	itType = data.tileTypes.begin();
 
-	for (int i = 0; i < data.height; i++)
-		for (int j = 0; j < data.width; j++)
-		{
-			// Diferenciamos si es sólido o no para el motor (type = 1 es base, es decir, solido)
-			if ((*itType) != 1) solid = 0; 
-			else solid = 1;
-			// Creamos la entidad y la metemos en el vector
-			// -1 -1 indican que no tienen idCollectable ni linkedTo
-			EntityTiled* e = new EntityTiled(entityType::TILEDENTITY, x + j, y + i, -1, -1, (*itTile), solid);
-			v.push_back(e);
+	int i = 0;
 
-			itTile++;
-			itType++;
-		}
+	while (itTile != data.tiles.end())
+	{
+		// cogemos cada tile y lo metemos en el vector
+		tiles[i] = (*itTile);
 
+		// cogemos el tipo de cada tile y lo metemos en el vector
+		tilesType[i] = (*itType);
+
+		itTile++;
+		itType++;
+		i++;
+	}
+			
+	// Creamos la entidad y la metemos en el vector
+	// -1 -1 indican que no tienen idCollectable ni linkedTo
+	return new EntityTiled(entityType::TILEDENTITY, x , y, -1, -1, data.tiles.size(), tiles, tilesType, data.width);
 /*
 	// para cada tile comprobamos el tipo y creamos la entidad que corresponda
 	while (itTile != data.tiles.end())
@@ -59,9 +64,7 @@ std::vector<Entity*> Decoration::toEntities()
 
 		itTile++;
 		itType++;
-	}
-
-*/	return v;
+	}*/
 };
 
 Decoration::DecorationData Decoration::getDecorationData()
