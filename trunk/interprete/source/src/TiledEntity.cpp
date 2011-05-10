dcb93bca8e50cd52d04612b1ba0b991b5ecde320
apsantorio <apsantorio@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-05-10 18:21:36 +0000

    + modificada la carga de TiledEntity + añadido funcionalidad en TiledEntity para que trabaje como SolidGrid de varios tiles

--- trunk/interprete/source/src/TiledEntity.cpp
+++ trunk/interprete/source/src/TiledEntity.cpp
@@ -3,21 +3,41 @@
 TiledEntity::TiledEntity(int x, int y, Game* g, GameState* gs) : Entity(x, y, g, gs)
 {
 	tileset = NULL;
-	tileId = NULL;
+	tilesId = NULL;
+	tilesType = NULL;
+	nTiles = 0;
+	width = 0;
+	height = 0;
 	solid = true;
 	depth = y;
 };
 
-void TiledEntity::init(TileSet* tset, int tile, bool foreground)
+void TiledEntity::init(TileSet* tset, short* tiles, short* tilesType, short nTiles, short width)
 {
-	tileset = tset;
-	tileId = tile;
+	this->nTiles = nTiles;
+	this->width = width;
+	this->tileset = tset;
+	this->tilesId = tiles;
+	this->tilesType = tilesType;
+	this->height = nTiles / width;
 
-	if (!foreground)
+	// Creamos la grid donde guardaremos los tilesType
+	int** grid = new int*[width];
+	for (int i = 0; i < width; i++)
+		grid[i] = new int[height];
+
+	// Rellenamos la grid
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++)
+			grid[j][i] = tilesType[i*width + j];
+
+	mask = new SolidGrid(x, y, grid, tileset->getTileW()*2, tileset->getTileH()*2, width, height);
+	
+	/*if (!foreground)
 		mask = new MaskBox(x, y, tileset->getTileW()*2, tileset->getTileH()*2, "semisolid");
 	else
 		depth += 1;
-		setCollidable(false);
+		setCollidable(false);*/
 };
 
 void TiledEntity::onRender()
@@ -27,5 +47,5 @@ void TiledEntity::onRender()
 
 	int tw = tileset->getTileW() * 2, th = tileset->getTileH() * 2;	// multiplicamos x2 para conseguir tamaño 16, que es el tamaño de las decoraciones
 	game->getGfxEngine()->renderPart(tileset->getImg(), x, y, 
-		tw*(tileId % (tileset->getColumns()/2)), th*(tileId / (tileset->getColumns()/2)), tw, th);
+		tw*(tilesId[0] % (tileset->getColumns()/2)), th*(tilesId[0] / (tileset->getColumns()/2)), width * tw, height * th);
 };
