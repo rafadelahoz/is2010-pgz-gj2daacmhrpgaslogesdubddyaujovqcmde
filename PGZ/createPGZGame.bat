579bb8a02337d3a15933fceaeb743d84bc561a07
ritrix <ritrix@b9ebc815-7a61-e10c-52ed-627adc48ba52>
2011-05-19 11:33:50 +0000

    Faltaba copiar el texto del prologo. Borrado cosas extra.

--- PGZ/createPGZGame.bat
+++ PGZ/createPGZGame.bat
@@ -1,13 +1,16 @@
 @echo off
 cd bin
+:: Start generator
 start /w generatorpgz.exe
+:: Copy extra stuff to data
 xcopy config-p1 .\data /C /R /I /Y
 xcopy config-p1-joy .\data /C /R /I /Y
+xcopy prologue.txt .\data /C /R /I /Y
 xcopy projectpgz-vs2010.exe ..\CreatedGame\ /C /R /I /Y
 xcopy data ..\CreatedGame\data\ /E /C /R /I /Y
 xcopy openal32.dll ..\CreatedGame\ /C /R /I /Y
 xcopy libsndfile-1.dll ..\CreatedGame\ /C /R /I /Y
-
+:: Execute created game
 cd ..\CreatedGame
 rename projectpgz-vs2010.exe game.exe
 start game.exe
